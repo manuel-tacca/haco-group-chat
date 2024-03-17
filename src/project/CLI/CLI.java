1b25e6ee995c56866023d8c6fd7565895b2ec449
LudoTassini <ludovica.tassini@mail.polimi.it>
2024-03-17 10:42:37 +0100

    added fix me

--- src/project/CLI/CLI.java
+++ src/project/CLI/CLI.java
@@ -78,7 +78,7 @@ public class CLI {
     public static void printRoomInfo(Room room){
         out.println("Room name: " + room.getName());
         out.println("Room ID: " + room.getIdentifier());
-        out.println("Room participants: " + room.getRoomMembers());
+        out.println("Room participants: " + room.getRoomMembers()); // FIXME: far vedere bene i nomi
     }
 
     public static void printRoomMessages(List<RoomText> roomTexts){
