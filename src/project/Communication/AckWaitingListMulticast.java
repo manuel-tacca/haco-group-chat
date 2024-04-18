b4a9b6bf98367444524cc1328672427581a0c946
manuel-tacca <manuel.tacca@mail.polimi.it>
2024-04-18 20:38:54 +0200

    rollback?

--- src/project/Communication/AckWaitingListMulticast.java
+++ src/project/Communication/AckWaitingListMulticast.java
@@ -2,26 +2,47 @@ package project.Communication;
 
 import java.io.IOException;
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 
 import project.CLI.CLI;
 import project.Communication.Messages.Message;
 
-public class AckWaitingListMulticast extends AckWaitingList{
+public class AckWaitingListMulticast {
 
+    private UUID ackWaitingListID;
     private int numberOfTotAcks;
     private int numberOfReceivedAcks;
-    private Message messageToResend;
     private Timer timer;
-    private Sender sender;
+    private TimerTask action;
+    private long delay;
 
     public AckWaitingListMulticast(UUID ackWaitingListID, Message messageToResend, int numberOfTotAcks, Sender sender) {
-        
-        super(ackWaitingListID, sender);
-        
+        this.ackWaitingListID = ackWaitingListID;
         this.numberOfTotAcks = numberOfTotAcks;
         this.numberOfReceivedAcks = 0;
-        this.messageToResend = messageToResend;
+        this.timer = new Timer();
+        this.action = new TimerTask() {
+            public void run() {
+                
+                CLI.printDebug("Action triggered!");
+                
+                if (numberOfTotAcks == numberOfReceivedAcks) {
+                    return;
+                }
+                
+                try {
+                    sender.sendMessage(messageToResend);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        this.delay = 1000;
+    }
+    
+    public void startTimer() {
+        timer.schedule(action, delay);
     }
 
     public void updateReceivedAcks() {
@@ -32,18 +53,7 @@ public class AckWaitingListMulticast extends AckWaitingList{
         }
     }
 
-	@Override
-	protected void onTimerExpired() {
-		CLI.printDebug("Action triggered!");
-                
-        if (numberOfTotAcks == numberOfReceivedAcks) {
-            return;
-        }
-        
-        try {
-            sender.sendMessage(messageToResend);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-	}
+    public UUID getAckWaitingListID() {
+        return ackWaitingListID; 
+    }
 }
--- src/project/Communication/AckWaitingListUnicast.java
+++ src/project/Communication/AckWaitingListUnicast.java
@@ -5,39 +5,53 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 
 import project.CLI.CLI;
 import project.Communication.Messages.Message;
+import project.Communication.Messages.MessageType;
 
-public class AckWaitingListUnicast extends AckWaitingList{
+public class AckWaitingListUnicast {
 
+    private UUID ackWaitingListID;
+    private MessageType messageType;
     private List<Message> messagesToResend; // maybe it should be done in client?
     private Timer timer;
     private Sender sender;
+    private TimerTask action;
+    private long delay;
 
     public AckWaitingListUnicast(UUID ackWaitingListID, List<Message> messagesToResend, Sender sender){
-        
-        super(ackWaitingListID, sender);
-
+        this.ackWaitingListID = ackWaitingListID;
         this.messagesToResend = new ArrayList<>();
         this.messagesToResend.addAll(messagesToResend);
-        
+        this.timer = new Timer();
+        this.sender = sender;
+        this.action = new TimerTask() {
+            public void run() {
+                CLI.printDebug("Action triggered!");
+                if (messagesToResend.size() == 0) {
+                    return;
+                }
+                for(Message m : messagesToResend) {
+                    try {
+                        sender.sendMessage(m);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        this.delay = 1000;
     }
     
-    @Override
-    protected void onTimerExpired() {   
-        CLI.printDebug("Action triggered!");
-        if (messagesToResend.size() == 0) {
-            return;
-        }
-        for(Message m : messagesToResend) {
-            try {
-                sender.sendMessage(m);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public UUID getAckWaitingListID() {
+        return ackWaitingListID;
+    }
+
+    public void startTimer() {
+        timer.schedule(action, delay);
     }
 
     public void remove(InetAddress address) {  // maybe it should be done in client ???
