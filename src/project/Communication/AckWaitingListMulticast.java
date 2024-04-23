d33cd04f3aedfd34aac814aa74d68cfdde3fc0b8
LudoTassini <ludovica.tassini@mail.polimi.it>
2024-04-23 18:11:29 +0200

    minor fix

--- src/project/Communication/AckWaitingListMulticast.java
+++ src/project/Communication/AckWaitingListMulticast.java
@@ -40,6 +40,7 @@ public class AckWaitingListMulticast extends AckWaitingList{
 
         ackingPeers.removeIf(p -> p.getIdentifier().toString().equals(peer.getIdentifier().toString()));
 
+
         if (ackingPeers.isEmpty()) {
             timer.cancel();
             CLI.printDebug("acks received successfully, stopping timer!");
